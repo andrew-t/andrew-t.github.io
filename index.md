4c71ce2bf6a6ace4215a6338d4045012885bdd09
Andrew Taylor <Andrew.Taylor04@bbc.co.uk>
2016-10-11 14:08:03 +0100

    Footnotes (needs transitions)

--- index.md
+++ index.md
@@ -4,7 +4,7 @@ layout: default
 
 # About
 
-I am a Manchester-based senior software developer, currently working for <a href="http://www.bbc.co.uk">the BBC</a> in MediaCityUK, in Salford Quays. I enjoy making Javascript do mathematics.
+I am a Manchester-based senior software developer, currently working for <a href="http://www.bbc.co.uk">the BBC</a> in MediaCityUK<span class="footnote" data-html="Which I inisit on pronouncing to rhyme with “audacity”, whatever anyone might think. If they want it pronounced as two words, they should put a space in it."></span>, in Salford Quays. I enjoy making Javascript do mathematics.
 
 <div class="float-left">
 Hello!
---
+++ js/footnote.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function(e) {
+	var elements = document.getElementsByClassName('footnote');
+
+	var container = document.createElement('div'),
+		body = document.createElement('div');
+	container.classList.add('hidden');
+	container.classList.add('footnote-body');
+	document.body.appendChild(container);
+	container.appendChild(body);
+
+	for (var i = 0; i < elements.length; ++i)
+		addFootnote(elements[i]);
+
+	function addFootnote(element) {
+		element.appendChild(document.createTextNode('*'));
+		element.addEventListener('click', function(e) {
+			body.innerHTML = element.getAttribute('data-html');
+			container.classList.remove('hidden');
+			e.preventDefault();
+		});
+	}
+
+	container.addEventListener('click', function(e) {
+		container.classList.add('hidden');
+		e.preventDefault();
+	});
+});
